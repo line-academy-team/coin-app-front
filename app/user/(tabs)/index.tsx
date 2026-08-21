12465f37065597d6f7f96686488f4be51539c412
mcm <joohyuna2@gmail.com>
2026-08-21 18:17:53 +0900

    메인페이지 진행중

--- app/user/(tabs)/index.tsx
+++ app/user/(tabs)/index.tsx
@@ -1,11 +1,35 @@
-import { ScrollView, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import { twMerge } from "tailwind-merge";
+import PortfolioSummary from "@/components/dashbaord/PortfolioSummary";
+import EmptyPortfolio from "@/components/dashbaord/Emptyportfolio";
 
- function Dashboard() {
+
+
+function Dashboard() {
+    const hasPortfolio = true;
+
     return (
         <View className={twMerge(["flex-1", "bg-background-default", "p-[30px]"])}>
-            <ScrollView>
-                <Text className={twMerge([""])}>안녕하세요</Text>
+            <ScrollView className="flex-1" contentContainerClassName="flex-grow">
+                <View className="flex-1 justify-between">
+                    <View>
+                        <Text
+                            className={twMerge(
+                                ["text-3xl", "font-pretendard-bold", "text-text-default"],
+                                ["py-5"],
+                            )}>
+                            안녕하세요
+                        </Text>
+                    </View>
+
+                    {hasPortfolio ? <PortfolioSummary /> : <EmptyPortfolio />}
+
+                    <View>
+                        <Button variant="solid" color="primary">
+                            + 포트폴리오 만들기
+                        </Button>
+                    </View>
+                </View>
             </ScrollView>
         </View>
     );
---
+++ components/dashbaord/PortfolioSummary.tsx
@@ -0,0 +1,52 @@
+import { Image, Text, View } from "react-native";
+import { twMerge } from "tailwind-merge";
+
+function PortfolioSummary() {
+    return (
+        <>
+            <View>
+                <Text
+                    className={twMerge([
+                        "text-text-secondary",
+                        "font-pretendard-semibold",
+                        "text-base",
+                    ])}>
+                    오늘의 포트폴리오 현황을 확인해보세요
+                </Text>
+            </View>
+            <View
+                className={twMerge(
+                    ["bg-primary-main", "rounded-xl", "p-3"],
+                    ["flex-row", "justify-between"],
+                )}>
+                <View>
+                    <Text className={twMerge(["font-pretendard-semibold text-text-light"])}>총자산</Text>
+                    <Text className={twMerge(["font-pretendard-semibold text-text-light"])}>￦32,430,250</Text>
+                    <Text className={twMerge(["font-pretendard-semibold text-text-light"])}>평가손익 +￦2,430,250</Text>
+                    <View className={"flex-row"}>
+                        <Text className={twMerge(["font-pretendard-semibold text-text-light"])}>수익률</Text>
+                        <View
+                            className={twMerge(
+                                ["text-text-light", "font-pretendard-semibold", "text-base"],
+                                ["p-3", "rounded-xl", "bg-[#FFFFFF/10]","border", "border-divider"],
+                            )}>
+                            +8.10%
+                        </View>
+                    </View>
+                </View>
+                <View>
+                    <Image
+                        source={require("@/assets/images/welcome/a7b6abd48871456077a8818d2955ed94772f99ec.png")}
+                        style={{
+                            width: 158,
+                            height: 105,
+                        }}
+                        resizeMode="contain"
+                    />
+                </View>
+            </View>
+        </>
+    );
+}
+
+export default PortfolioSummary;
